745781e3751ec67cfa1403d7f6c95af1fdf16f9a
adomin <adomin@163.com>
2018-09-23 23:41:30 +0800

    mock data chatterList

--- src/view/workIM/ChatterList.js
+++ src/view/workIM/ChatterList.js
@@ -1,76 +1,44 @@
 import React, { Component } from 'react'
 import { List, Avatar } from 'antd'
+import { messageList, user } from '@/mock/imData'
 
 export default class ChatterList extends Component {
   constructor (props, context) {
     super(props)
     this.state = {
-      currentId: null
+      currentId: null,
+      chartterList: []
     }
     this.handleOnClickChatter = this.handleOnClickChatter.bind(this)
+    this.getChartterList = this.getChartterList.bind(this)
   }
 
   handleOnClickChatter (id) {
     this.setState({ currentId: id })
   }
 
-  render () {
-    const data = [
-      {
-        id: 1,
-        title: '玩家1'
-      },
-      {
-        id: 2,
-        title: '玩家2'
-      },
-      {
-        id: 3,
-        title: '玩家3'
-      },
-      {
-        id: 4,
-        title: '玩家4'
-      },
-      {
-        id: 5,
-        title: '玩家1'
-      },
-      {
-        id: 6,
-        title: '玩家2'
-      },
-      {
-        id: 7,
-        title: '玩家3'
-      },
-      {
-        id: 8,
-        title: '玩家4'
-      },
-      {
-        id: 9,
-        title: '玩家1'
-      },
-      {
-        id: 10,
-        title: '玩家2'
-      },
-      {
-        id: 11,
-        title: '玩家3'
-      },
-      {
-        id: 12,
-        title: '玩家4'
+  componentDidMount () {
+    this.getChartterList(messageList)
+  }
+
+  getChartterList (messageList) {
+    let chartterList = []
+    messageList.forEach(obj => {
+      if (chartterList.indexOf(obj.threadId) === -1) {
+        chartterList.push(obj.threadId)
       }
-    ]
+    })
+    this.setState({ currentId: chartterList[0] })
+    this.state.chartterList = chartterList.map(item => user[item])
+  }
+
+  render () {
     const imgUrl = require('@/assets/11.jpg')
-    const { currentId } = this.state
+    const { currentId, chartterList } = this.state
     return (
       <List
         itemLayout="horizontal"
-        dataSource={data}
+        dataSource={chartterList}
         renderItem={item => (
           <List.Item
             style={{ padding: '12px 20px', cursor: 'default', borderBottom: '1px solid #e0e0e0' }}
@@ -78,7 +46,7 @@ export default class ChatterList extends Component {
             onClick={() => this.handleOnClickChatter(item.id)}>
             <List.Item.Meta
               avatar={<Avatar shape="square" size={42} src={imgUrl} />}
-              title={<span style={{ fontSize: '14px' }}>{item.title}</span>}
+              title={<span style={{ fontSize: '14px' }}>{item.charName}</span>}
               description={<span style={{ color: '#aaa', fontSize: '12px' }}>游戏服王霸天下</span>}
             />
           </List.Item>
